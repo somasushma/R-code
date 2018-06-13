d6853c20ed41c029567ce4cffc1efab97bbde709
somasushma <39967258+somasushma@users.noreply.github.com>
2018-06-13 00:31:49 -0400

    Update magic_square4x4.R

--- scripts/magic_square4x4.R
+++ scripts/magic_square4x4.R
@@ -1,6 +1,6 @@
 # how to make 4 x 4 magic square
 # first make below basic square
-m=matrix(data=c(8,11,14,1,13,2,7,12,3,16,9,6, 10,5,4,15), nrow = 4, byrow = T)
+m=matrix(data=c(8,11,14,1,13,2,7,12,3,16,9,6,10,5,4,15), nrow = 4, byrow = T)
 s1=sum(m[,1])
 #choose sum (e.g. 64 for kaumara square)
 s2=64
