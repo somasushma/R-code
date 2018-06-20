8d1ba274bf4cd729761f144e9da64573d4189d08
somasushma <39967258+somasushma@users.noreply.github.com>
2018-06-19 21:47:11 -0400

    Add files via upload

--- scripts/Stern_array.R
+++ scripts/Stern_array.R
@@ -20,9 +20,16 @@ for(j in 2:n){
   f2[[j]][seq(2,(l-1),2)]=sapply(1:(length(f2[[j-1]])-1), function(x) f2[[j-1]][x]+f2[[j-1]][x+1])  
 }
 
+#plot as points
 par(mfrow=c(1,1))
 par(pty="m", mar=c(2,2,1,1), mgp=c(1,.35,0))
-plot(unlist(f1), pch=16, cex=.25, col=c("dodgerblue4","darkred"), xlab="n",ylab= expression(paste(f,"[n]")), main=expression(paste(f, " so-called Farey denominators")))
+plot(unlist(f1), pch=16, cex=.25, col=c("dodgerblue4","darkred"), xlab="n",ylab= expression(paste(f,"[n]")), main=expression(paste(f, " Stern denominators")))
 
 #as continuous
-plot(unlist(f1), type="l", cex=.25, col=c("dodgerblue4","darkred"), xlab="n",ylab= expression(paste(f,"[n]")), main=expression(paste(f, " so-called Farey denominators")))
+plot(unlist(f1), type="l", cex=.25, col=c("dodgerblue4","darkred"), xlab="n",ylab= expression(paste(f,"[n]")), main=expression(paste(f, " Stern numerators")))
+
+#last cycle of denominators
+plot(unlist(f1[n]), type="l", cex=.25, col=c("dodgerblue4","darkred"), xlab="n",ylab= expression(paste(f,"[n]")), main=substitute(paste("Stern denominators, cycle ", n), list(n=n)))
+
+#plot Stern fractions
+plot(unlist(f2[n])/unlist(f1[n]), type="l", cex=.25, col=c("dodgerblue4","darkred"), lwd=2, xlab="n",ylab= expression(paste(f,"[n]")), main=expression(paste(f, " Stern fraction sequence")))
