677496f0dca7595eff2ed7b682d1a716cca4d1bf
somasushma <39967258+somasushma@users.noreply.github.com>
2018-11-29 15:43:20 -0500

    Add files via upload

--- scripts/basen_periodic_derivative.R
+++ scripts/basen_periodic_derivative.R
@@ -12,8 +12,8 @@ nnary=function(x,y){
   }
 }
 
-l=4 #base
-pow=8
+l=5 #base
+pow=6
 n=l^pow
 f1=sapply(1:n, function(x) nnary(l,x)) #base n
 
@@ -21,19 +21,21 @@ f1=sapply(1:n, function(x) nnary(l,x)) #base n
 f2=vector(n,mode = "list")
 for (j in 1:length(f1)) {
   if(length(f1[[j]])==1){
-    f2[[j]]=f1[[j]]
+    f2[[j]]=(f1[[j]]+f1[[j]]) %% l
   } else {
     f2[[j]]=c(sapply(1:(length(f1[[j]])-1), function(x) (f1[[j]][x]+f1[[j]][x+1]) %% l), ((tail(f1[[j]],1)+head(f1[[j]],1)) %% l))
   }
 }
 
-#periodic derivative as matrix plot
+#periodic derivative as matrix plot compared with n-base numbers
 m=lengths(tail(f2,1))
 pm=sapply(f2, function(x) c(rep(0, m - length(x)), x))
 pm=t(pm)
-
+bm=sapply(f1, function(x) c(rep(0, m - length(x)), x))
+bm=t(bm)
+pm=cbind(pm,bm)
 par(pty="m", mar=c(2,2,1,1), mgp=c(1,.35,0))
-image(pm, asp=m/n, col = terrain.colors(5), axes=F)
+image(pm, asp=(2*m)/n, col = topo.colors(l), axes=F)
 
 #base b to base 10 converter
 bto10=function(x,b){
@@ -49,11 +51,29 @@ bto10=function(x,b){
 
 f3=unlist(lapply(f2, function(x) as.numeric(paste(x, collapse = "")))) #convert to number
 f3=sapply(f3, function(x) bto10(x,l))
+#plot
+par(pty="m", mar=c(2,2,1,1), mgp=c(1,.35,0))
+plot(f3, type="n", col=c("#e41a1c","#377eb8", "#4daf4a"), xlab="n",ylab= substitute(paste(f,"[n]")))
+abline(v=sapply(1:pow, function(x) l^x), h=0, col="blue", lty=3)
+abline(h=sapply(1:pow, function(x) l^x), col="blue", lty=3)
+points(f3, type="l", col=c("#e41a1c","#377eb8", "#4daf4a"), xlab="n",ylab= substitute(paste(f,"[n]")))
 
-plot(f3, type="l", col=c("#e41a1c","#377eb8", "#4daf4a"), xlab="n",ylab= substitute(paste(f,"[n]")))
-abline(v=sapply(1:pow, function(x) l^x), h=0, col="blue", lty=2)
+#plot as square
+par(pty="s", mar=c(2,2,1,1), mgp=c(1,.35,0))
+plot(f3, type="n", col=c("#e41a1c","#377eb8", "#4daf4a"), xlab="n",ylab= substitute(paste(f,"[n]")))
+abline(v=sapply(1:pow, function(x) l^x), h=0, col="blue", lty=3)
+abline(h=sapply(1:pow, function(x) l^x), col="blue", lty=3)
+points(f3, type="l", lwd=2, col=c("#e41a1c","#377eb8", "#4daf4a"), xlab="n",ylab= substitute(paste(f,"[n]")))
 
-which(f3==0 | f3==1 | f3==2)
+#minima
+which(f3==0 | f3==1 | f3==2 |f3==3) #locating minima
 l^(1:pow)
-cons=2-(30583+1)/16384
-#k=2-(ln1+1)/4^(n-1)
+
+consmin=(l-2)/(l^2-1) #direct formula for first minimum constant
+
+#k=2-(min_n1+1)/base^(n-1)
+ceiling(l^(1:pow)*(2-consmin)-1) #recapitulating first min
+
+#maxima
+consmax=2/(l+1) #formula for first maximum constant
+ceiling(l^(1:pow)*(2-consmax)-1) #recapitulating first max
