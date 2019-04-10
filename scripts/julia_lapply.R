f5fad40eea91cd66d035a765d0060b65a4a412e1
somasushma <39967258+somasushma@users.noreply.github.com>
2019-04-10 15:30:04 -0400

    Add files via upload

--- scripts/julia_lapply.R
+++ scripts/julia_lapply.R
@@ -7,9 +7,16 @@ f.list$c3_2julia=function(z,c) z^3-(-z)^(2.00001)+c
 
 f.list$julinvpow2=function(z,c) z^(-2)+c
 
+f.list$julpow2_inv1=function(z,c) z^2+c/z
+
+f.list$julpow2_inv2=function(z,c) z^2+c/z^2
+
 col.list=list()
 col.list$gray=gray.colors
 col.list$CyBldrBl=colorRampPalette(c("cyan2", "black","darkblue"))
+col.list$TudrOldrBl=colorRampPalette(c("darkturquoise", "darkred","darkblue"))
+col.list$WOdR=colorRampPalette(c("white","orange","darkred"))
+col.list$dyad=c("black", "white")
 
 julia=function(z, n, c){
   for(j in 1:n){
@@ -22,15 +29,16 @@ julia=function(z, n, c){
 }
 
 
-
-fz=f.list$julinvpow2
-col=col.list$CyBldrBl
+fz=f.list$julpow2_inv2
+col=col.list$TudrOldrBl
 n=100
-c=-1.2
+c=-.1
 b=1.5
-dv=.005
+dv=.0025
 pm= as.vector(outer(X = seq(-b,b,dv), Y = seq(-b,b,dv)*1i,FUN = "+"))
 cm=sapply(pm, function(x) julia(x,n, c))
-par(mar=c(2,2,2,1), mgp=c(1.1,.4,0), pty="s")
 
-plot(pm, pch=16, cex=.1, col=rev(col(100))[cm], xlab = "x", ylab = "iy", main=substitute(paste("f(z)=",f, "; c=", c), list(c=c, f=body(fz))))
+png(filename = "~/R/Figures/Figures1/Jul020.png", height = 1500, width = 1500 )
+par(mar=c(2,2,2,1), mgp=c(1.1,.4,0), pty="s")
+plot(pm, pch=16, cex=.1, col=col(100)[cm], xlab = "x", ylab = "iy", main=substitute(paste("f(z)=",f, "; c=", c), list(c=c, f=body(fz))), cex.main=2)
+dev.off()
