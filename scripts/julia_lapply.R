919e3a77b413e5e91fe1f713b9684d07ba79dd53
somasushma <39967258+somasushma@users.noreply.github.com>
2019-04-09 22:01:32 -0400

    Update julia_lapply.R

--- scripts/julia_lapply.R
+++ scripts/julia_lapply.R
@@ -1,16 +1,19 @@
+f.list=list()
+f.list$julpow2=function(z,c) z^2+c
+
+f.list$cosjul=function(z,c) c*cos(z)
+
+f.list$c3_2julia=function(z,c) z^3-(-z)^(2.00001)+c
+
+f.list$julinvpow2=function(z,c) z^(-2)+c
+
+col.list=list()
+col.list$gray=gray.colors
+col.list$CyBldrBl=colorRampPalette(c("cyan2", "black","darkblue"))
+
 julia=function(z, n, c){
   for(j in 1:n){
-    z=z^2+c
-    if(Mod(z) >16 || Mod(z)==Inf){
-     break
-    }
-  }
-  return(j)
-}
-
-cosjulia=function(z, n, c){
-  for(j in 1:n){
-    z=c*cos(z)
+    z=fz(z,c)
     if(Mod(z) >16 || Mod(z)==Inf){
       break
     }
@@ -18,34 +21,16 @@ cosjulia=function(z, n, c){
   return(j)
 }
 
-c3julia=function(z, n, c){
-  for(j in 1:n){
-    z=z^3-(-z)^(2.00001)+c
-    if(Mod(z) >16 || Mod(z)==Inf){
-      break
-    }
-  }
-  return(j)
-}
 
-fracjulia=function(z, n, c){
-  for(j in 1:n){
-    z=z^(-2)+c
-    if(Mod(z) >16 || Mod(z)==Inf){
-      break
-    }
-  }
-  return(j)
-}
 
+fz=f.list$julinvpow2
+col=col.list$CyBldrBl
 n=100
-c=-2.95
-b=pi
+c=-1.2
+b=1.5
 dv=.005
 pm= as.vector(outer(X = seq(-b,b,dv), Y = seq(-b,b,dv)*1i,FUN = "+"))
-cm=sapply(pm, function(x) cosjulia(x,n, c))
+cm=sapply(pm, function(x) julia(x,n, c))
 par(mar=c(2,2,2,1), mgp=c(1.1,.4,0), pty="s")
-# plot(pm, pch=16, cex=.1, col=gray.colors(100)[cm], xlab = "x", ylab = "iy", main=paste("c=", c))
 
-# col=colorRampPalette(c("cyan2", "black","darkblue"))
-plot(pm, pch=16, cex=.1, col=col(100)[cm], xlab = "x", ylab = "iy", main=paste("c=", c))
+plot(pm, pch=16, cex=.1, col=rev(col(100))[cm], xlab = "x", ylab = "iy", main=substitute(paste("f(z)=",f, "; c=", c), list(c=c, f=body(fz))))
