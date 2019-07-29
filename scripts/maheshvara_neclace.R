1343bb9af03d5024931ee748558fad50a4f73ac2
somasushma <39967258+somasushma@users.noreply.github.com>
2019-07-29 13:45:09 -0400

    Update maheshvara_neclace.R

--- scripts/maheshvara_neclace.R
+++ scripts/maheshvara_neclace.R
@@ -27,17 +27,17 @@ plot(te, pch=16 )
 abline(a = 0, b=2.2)
 abline(a = 0, b=1/2.3)
 
-
+#table of frequencies
 te=sort(table(unlist(fs)), decreasing = T)
 knitr::kable(te,format = "pandoc")
 
-which(unlist(lapply(1:n, function(x) "5882353" %in% fs[[x]])))
+which(unlist(lapply(1:n, function(x) "1058313049" %in% fs[[x]])))
 
 forlist=list()
 forlist$`11`=function(x) 2*x+1
 forlist$`101`=function(x) 4*x+2
-forlist$`13`=function(x) 6*x+3
 forlist$`7`=function(x) 6*x+3
+forlist$`13`=function(x) 6*x+3
 forlist$`73`=function(x) 8*x+4
 forlist$`137`=function(x) 8*x+4
 forlist$`9091`=function(x) 10*x+5
@@ -49,8 +49,34 @@ forlist$`19`=function(x) 18*x+9
 forlist$`52579`=function(x) 18*x+9
 forlist$`3541`=function(x) 20*x+10
 forlist$`27961`=function(x) 20*x+10
+forlist$`23`=function(x) 22*x+11
+forlist$`4093`=function(x) 22*x+11
+forlist$`8779`=function(x) 22*x+11
+forlist$`99990001`=function(x) 24*x+12
+forlist$`859`=function(x) 26*x+13
+forlist$`1058313049`=function(x) 26*x+13
+forlist$`29`=function(x) 28*x+14
+forlist$`281`=function(x) 28*x+14
+forlist$`121499449`=function(x) 28*x+14
+forlist$`211`=function(x) 30*x+15
+forlist$`241`=function(x) 30*x+15
+forlist$`2161`=function(x) 30*x+15
+forlist$`353`=function(x) 32*x+16
+forlist$`449`=function(x) 32*x+16
+forlist$`641`=function(x) 32*x+16
+forlist$`1409`=function(x) 32*x+16
+forlist$`69857`=function(x) 32*x+16
 
-
+#reconstruct factors
+ind.list=vector(mode = "list", length = length(forlist))
+for(j in 1:length(forlist)){
+  te=paste(body(forlist[[j]]), collapse = "")
+  te=unlist(strsplit(te, "\\*|\\+"))
+  te=as.numeric(te[2])
+  f=forlist[[j]]
+  ind.list[[j]]=f(0:floor(50/te))
+}
+names(ind.list)=names(forlist)
 
 #factor network--------------
 l=lengths(fs)
@@ -87,4 +113,8 @@ tkplot(g, layout=norm_coords(layout_components(g,layout = layout_with_kk), xmin=
 
 plot.igraph(g, layout=norm_coords(layout_components(g,layout = layout_with_fr),xmin = 1,xmax = 20, ymin = 1,ymax = 15))
 
-
+#separating families
+fams= components(g)
+te=names(fams$membership[fams$membership==3])
+ye=unique(unlist(fs))
+ge=ye[ye %in% te]
