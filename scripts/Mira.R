34047eb29b2f76692274bd782fb20de6156f7828
somasushma <39967258+somasushma@users.noreply.github.com>
2020-07-21 23:42:34 -0400

    Update Mira.R

--- scripts/Mira.R
+++ scripts/Mira.R
@@ -1,3 +1,60 @@
+library(readr)
+
+windowsFonts(f1 = windowsFont("Constantia"),
+             f2 = windowsFont("Book Antiqua"),
+             f3 = windowsFont("Cambria Math"))
+
+Mira <- read_delim("~/cutting_block/R/data_analysis/data/Mira.tsv", 
+                   "\t", escape_double = FALSE, trim_ws = TRUE)
+Mira = data.frame(Mira)
+
+par(mar=c(2,2,2,1), mgp=c(1.1,.4,0), mfrow=c(1,1))
+hist(Mira$Period, breaks = 30, col = gray.colors(31), main= "Mira periods", xlab = "Mira periods", family="f3")
+grid(col="gray30")
+abline(v=c(mean(Mira$Period), median(Mira$Period)), col=c("darkgreen", "darkred"), lwd=2, lty=3)
+box()
+
+Mira$magDiff = Mira$magMin - Mira$magMax
+
+par(mar=c(2,2,2,1), mgp=c(1.1,.4,0), mfrow=c(1,1))
+hist(Mira$magDiff, breaks = 30, col = gray.colors(31), main= "Mira magnitude amplitudes", xlab = "Mira magnitude amplitudes", family="f3")
+grid(col="gray30")
+box()
+
+#convert to galactic
+a0=192.8595*pi/180
+d0=27.1284*pi/180
+l0=122.9320*pi/180
+galc=function(a,d){
+  a=a*pi/180
+  d=d*pi/180
+  y=(cos(d)*sin(a-a0))
+  x=(sin(d)*cos(d0)-cos(d)*sin(d0)*cos(a-a0))
+  l=l0-atan2(y,x)
+  l=l*180/pi
+  l[which(l<0)] = l[which(l<0)]+360
+  y=sin(d)*sin(d0)+cos(d)*cos(d0)*cos(a-a0)
+  b=asin(y)
+  b=b*180/pi
+  return(cbind(l,b))
+}
+
+#Hammer-Aitoff projection
+
+ham.ait=function(l,b){
+  l[which(l>180)] = l[which(l>180)]-360
+  x=sqrt(8)*cos(b*pi/180)*sin(l*pi/360)/sqrt(1+cos(b*pi/180)*cos(l*pi/360))
+  y=sqrt(2)*sin(b*pi/180)/sqrt(1+cos(b*pi/180)*cos(l*pi/360))
+  return(cbind(x,y))
+}
+
+
+ham.ait=function(l,b){
+  l[which(l>180)] = l[which(l>180)]-360
+  x=sqrt(8)*cos(b*pi/180)*sin(l*pi/360)/sqrt(1+cos(b*pi/180)*cos(l*pi/360))
+  y=sqrt(2)*sin(b*pi/180)/sqrt(1+cos(b*pi/180)*cos(l*pi/360))
+  return(cbind(x,y))
+}
 
 #boundary
 bdx=c(rep(180.0001,90), rep(180,90))
